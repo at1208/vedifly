a479b172e202e9a23b73ee07146b96e94259eb28
at1208 <amantiwari12081999@gmail.com>
2021-08-11 09:00:33 +0530

    fix: schema

--- components/Blog/blogSchema.js
+++ components/Blog/blogSchema.js
@@ -1,29 +1,31 @@
 const BlogSchema = (blog) => {
-  return { "@context": "https://schema.org",
-  "@type": "BlogPosting",
-  "mainEntityOfPage": {
-    "@type": "WebPage",
-    "@id": `${process.env.NEXT_PUBLIC_DOMAIN}/${blog.slug}`
-  },
-  "headline": blog.title,
-  "image": `${process.env.NEXT_PUBLIC_API}/${blog.featureImg}`,
-  "author": {
-    "@type": "Person",
-    "name": blog.postedBy.full_name
-  },
-  "publisher": {
-    "@type": "Organization",
-    "name": "Vedifly.com",
-    "logo": {
-      "@type": "ImageObject",
-      "url": "https://vedifly.com/vedifly.png",
-      "width": 60,
-      "height": 60
-    }
-  },
-  "datePublished": blog.createdAt,
-  "dateModified": blog.updatedAt
-  }
-}
+  return {
+    "@context": "https://schema.org",
+    "@type": "BlogPosting",
+    mainEntityOfPage: {
+      "@type": "WebPage",
+      "@id": `${process.env.NEXT_PUBLIC_DOMAIN}/${blog.slug}`,
+    },
+    headline: blog.title,
+    image: `${blog.featureImg}`,
+    author: {
+      "@type": "Person",
+      name: blog.postedBy.full_name,
+      url: "",
+    },
+    publisher: {
+      "@type": "Organization",
+      name: "Vedifly.com",
+      logo: {
+        "@type": "ImageObject",
+        url: "https://vedifly.com/vedifly.png",
+        width: 60,
+        height: 60,
+      },
+    },
+    datePublished: blog.createdAt,
+    dateModified: blog.updatedAt,
+  };
+};
 
 export default BlogSchema;
